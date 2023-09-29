cf9f0b6f8027abb4b8762b8e3e93d16c79fc140e
Akkey <146309494+Aky-0114@users.noreply.github.com>
2023-09-29 15:05:59 +0900

    1.0.0

---
+++ stake/__init__.py
@@ -0,0 +1,8 @@
+__title__ = "stake.py"
+__author__ = "akkey"
+__license__ = "MIT"
+__copyright__ = "Copyright (C) 2023 Akkey0114"
+__version__ = "1.0.0"
+
+from .stake import Stake
+from .stake import StakeSocket
